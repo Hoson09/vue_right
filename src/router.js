b99a668df6a1b0333ab005c7d6071f8e4ed9062f
George_hu <huygeorge@163.com>
2019-09-04 15:25:27 +0800

    commit iview

--- src/router.js
+++ src/router.js
@@ -1,6 +1,6 @@
 import Vue from "vue";
 import Router from "vue-router";
-import Home from "./views/Home.vue";
+import MainView from "./views/MainView";
 
 Vue.use(Router);
 
@@ -8,8 +8,8 @@ export default new Router({
   routes: [
     {
       path: "/",
-      name: "home",
-      component: Home
+      name: "mainview",
+      component: MainView
     },
     {
       path: "/about",
---
+++ src/views/MainView.vue
@@ -0,0 +1,10 @@
+<template>
+  <div class="mainview"></div>
+</template>
+
+<script>
+export default {
+  name: "mainview",
+  components: {}
+};
+</script>
