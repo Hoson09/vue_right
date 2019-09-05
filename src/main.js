937e50825c66916839499cb3b47debec41845685
lcj <843652590@qq.com>
2019-09-05 10:48:57 +0800

    99

--- src/main.js
+++ src/main.js
@@ -1,26 +1,10 @@
-<<<<<<< HEAD
 /*
  * @Description: In User Settings Edit
  * @Author: your name
- * @Date: 2019-09-04 14:14:00
- * @LastEditTime: 2019-09-04 20:18:29
+ * @Date: 2019-09-05 10:38:24
+ * @LastEditTime: 2019-09-05 10:48:34
  * @LastEditors: Please set LastEditors
  */
-import Vue from "vue";
-import App from "./App.vue";
-import router from "./router";
-import store from "./store";
-import "./plugins/element.js";
-import "./plugins/iview.js";
-
-Vue.config.productionTip = false;
-
-new Vue({
-  router,
-  store,
-  render: h => h(App)
-}).$mount("#app");
-=======
 import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
@@ -37,4 +21,3 @@ new Vue({
   store,
   render: h => h(App)
 }).$mount("#app");
->>>>>>> a96ebf0a51a2796d6ad048421d2ee7f7a9e3f111
