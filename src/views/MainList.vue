d2cf9b791ba4371e9efe8b49f151b22d7c323bec
George_hu <huygeorge@163.com>
2019-09-07 16:58:47 +0800

    add MainList.vue

---
+++ src/views/MainList.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="mainlist">
+    <h1>请在右侧菜单列表选择你需要的选项</h1>
+  </div>
+</template>
+<script>
+export default {
+  name: "mainlist",
+  data() {
+    return {};
+  }
+};
+</script>
+<style scoped lang="scss">
+.mainlist {
+  height: 100%;
+  width: 100%;
+  h1 {
+    margin: 200px auto;
+    text-align: center;
+    font-size: 40px;
+  }
+}
+</style>
